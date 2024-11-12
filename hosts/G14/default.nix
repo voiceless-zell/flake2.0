5c4c07a313a173a346e23a6d960df2c749b176b6
voiceless-zell <peter.bouchard2893@proton.me>
2024-11-12 08:19:42 -0500

    added hosts

---
+++ hosts/G14/default.nix
@@ -0,0 +1,68 @@
+{
+  pkgs,
+  config,
+  ...
+}:
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernel.sysctl = {
+    "vm.max_map_count" = 20971520;
+    "fs.file-max" = 524288;
+  };
+  #boot.initrd.preDeviceCommands = ''
+  #  DEVS="0000:01:00.0 0000:01:00.1"
+  #  for DEV in $DEVS; do
+  #    echo "vfio-pci" > /sys/bus/pci/devices/$DEV/driver_override
+  #  done
+  #  modprobe -i vfio-pci
+  #'';
+  boot.kernelParams = [
+    "intel_iommu=on"
+    "pcie_aspm=off"
+  ];
+  boot.initrd.availableKernelModules = [ "vfio-pci" ];
+  networking = {
+    hostName = "G14";
+    networkmanager.enable = true;
+    firewall.enable = false;
+  };
+  zramSwap.enable = true;
+  services.xserver = {
+    enable = true;
+    xkb.layout = "us";
+    videoDrivers = [
+      "amdgpu"
+      "nvidia"
+    ];
+  };
+  hardware.nvidia = {
+    package = config.boot.kernelPackages.nvidiaPackages.production;
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    dynamicBoost.enable = true;
+    nvidiaSettings = true;
+    prime = {
+      offload = {
+        enable = true;
+        enableOffloadCmd = true;
+      };
+      amdgpuBusId = "PCI:04:00:0";
+      nvidiaBusId = "PCI:01:00:0";
+    };
+  };
+  services.asusd.enable = true;
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-compute-runtime
+      intel-media-driver
+    ];
+  };
+  powerManagement.cpuFreqGovernor = "performance";
+  # services.tailscale = {
+  #   enable = true;
+  #   useRoutingFeatures = "client";
+  #   };
+}
---
+++ hosts/P16/default.nix
@@ -0,0 +1,76 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+{
+  imports = [
+    ./hardware-configuration.nix
+  ];
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernel.sysctl = {
+    "vm.max_map_count" = 16777216;
+    "fs.file-max" = 524288;
+  };
+  networking = {
+    hostName = "P16";
+    networkmanager.enable = true;
+    firewall.enable = false;
+  };
+  services.xserver = {
+    enable = true;
+    xkb.layout = "us";
+    videoDrivers = [
+      "nvidia"
+      "inter"
+    ];
+    displayManager.sddm = {
+      enable = true;
+    };
+    desktopManager.gnome = {
+      enable = true;
+    };
+  };
+
+  services.libinput = {
+    enable = true;
+    mouse = {
+      accelProfile = "flat";
+    };
+  };
+
+  services.displayManager.sddm.wayland.enable = true;
+  services.gvfs.enable = true;
+  services.hardware.bolt.enable = true;
+  services.xserver.displayManager.sessionCommands = ''
+    ${lib.getBin pkgs.xorg.xrandr}/bin/xrandr --setprovideroutputsource 2 0
+  '';
+  hardware.nvidia = {
+    modesetting.enable = false;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    nvidiaSettings = true;
+    open = true;
+    prime = {
+      sync.enable = true;
+      intelBusId = "PCI:0:02:0";
+      nvidiaBusId = "PCI:1:00:0";
+    };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-compute-runtime
+      intel-media-driver
+      mesa
+      intel-vaapi-driver
+      vaapiVdpau
+    ];
+  };
+  powerManagement.cpuFreqGovernor = "performance";
+  # services.tailscale = {
+  #   enable = true;
+  #   useRoutingFeatures = "client";
+  #   };
+}
