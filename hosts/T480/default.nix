a1b4b2b8ce4edb9e0da7d658aacd69dc8eaa5621
voiceless-zell <peter.bouchard2893@proton.me>
2024-11-12 08:37:57 -0500

    add T480

---
+++ hosts/T480/default.nix
@@ -0,0 +1,48 @@
+{
+  pkgs,
+  nixpkgs,
+  inputs,
+  config,
+  ...
+}:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./../../nixos
+  ];
+  boot.kernelModules = [ "kvm-intel" ];
+  nixpkgs.config.allowUnfree = true;
+  networking = {
+    hostName = "T480";
+  };
+  services.xserver = {
+    enable = true;
+    xkb.layout = "us";
+    videoDrivers = [ "nvidia" ];
+    desktopManager.gnome = {
+      enable = true;
+    };
+    libinput = {
+      enable = true;
+      mouse = {
+        accelProfile = "flat";
+      };
+    };
+  };
+  hardware = {
+    opengl = {
+      enable = true;
+      driSupport = true;
+      driSupport32Bit = true;
+      extraPackages =
+        with pkgs;
+        [
+        ];
+    };
+  };
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+  services.gvfs.enable = true;
+}
