bee524f37fd4b61fd0e450309bb3672174ee71f3
voiceless-zell <peter.bouchard2893@proton.me>
2024-11-13 17:49:18 -0500

    fix to monitor

--- hosts/T480/home.nix
+++ hosts/T480/home.nix
@@ -3,7 +3,11 @@
   ...
 }:
 {
-  home =
-    {
+  home = {
+    home-manager.user.zell = {
+      wayland.windowManager.hyprland.settings.monitor = [
+        "eDP=1,1920x1080,0x0,1"
+      ];
     };
+  };
 }
