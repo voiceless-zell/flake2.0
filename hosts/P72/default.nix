fbe95a332b5c259a79b8adbf315e188fb9bc789a
voiceless-zell <peter.bouchard2893@proton.me>
2024-11-13 20:19:03 -0500

    add nvidia open

--- hosts/P72/default.nix
+++ hosts/P72/default.nix
@@ -1,33 +1,41 @@
-{pkgs, inputs, config, nixpkgs, self, ...}: 
-{        
- 
-boot.kernelModules = [ "intel" ];
-boot.loader.efi.canTouchEfiVariables = true;
-boot.kernel.sysctl = {
-  "vm.max_map_count" = 16777216;
-  "fs.file-max" = 524288;
-};
+{
+  pkgs,
+  inputs,
+  config,
+  nixpkgs,
+  self,
+  ...
+}:
+{
+
+  boot.kernelModules = [ "intel" ];
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernel.sysctl = {
+    "vm.max_map_count" = 16777216;
+    "fs.file-max" = 524288;
+  };
   networking = {
     hostName = "P72";
     networkmanager.enable = true;
     firewall.enable = false;
   };
-   services.xserver = {
+  services.xserver = {
     enable = true;
     xkb.layout = "us";
     videoDrivers = [ "nvidia" ];
   };
- hardware.nvidia = {
-     modesetting.enable = true;
+  hardware.nvidia = {
+    open = true;
+    modesetting.enable = true;
     powerManagement.enable = false;
     powerManagement.finegrained = false;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
-     prime ={
+    prime = {
       sync.enable = true;
       intelBusId = "PCI:1:00:0";
       nvidiaBusId = "PCI:1:00:0";
-     };   
+    };
   };
   hardware.opengl = {
     enable = true;
