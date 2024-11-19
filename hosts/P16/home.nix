036faa5de58de822fdbced0ebd3fb4fe5b5efc7a
voiceless-zell <peter.bouchard2893@proton.me>
2024-11-19 16:57:08 -0500

    add packackages move discord to host import

--- hosts/P16/home.nix
+++ hosts/P16/home.nix
@@ -1,13 +1,23 @@
 {
-  config,
-  lib,
-  user,
+  pkgs,
+  inputs,
   ...
 }:
 {
   config = {
     wayland.windowManager.hyprland.settings.monitor = [
       "eDP-1,2560x1600,0x0,1"
+    ];
+    home.packages = with pkgs; [
+      discord
+      inputs.nix-citizen.packages.${system}.star-citizen
+      inputs.nix-citizen.packages.${system}.star-citizen-helper
+      inputs.nix-citizen.packages.${system}.lug-helper
+      lutris
+      wineWowPackages.waylandFull
+      winetricks
+      gamescope
+      prismlauncher
     ];
   };
 }
