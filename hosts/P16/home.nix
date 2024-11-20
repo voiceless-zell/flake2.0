11acd1275e407ef89ffd638cf7b05ff818fb190d
voiceless-zell <peter.bouchard2893@proton.me>
2024-11-20 06:20:24 -0500

    adding pkgs

--- hosts/P16/home.nix
+++ hosts/P16/home.nix
@@ -9,6 +9,7 @@
       "eDP-1,2560x1600,0x0,1"
     ];
     home.packages = with pkgs; [
+      protontricks
       discord
       inputs.nix-citizen.packages.${system}.star-citizen
       inputs.nix-citizen.packages.${system}.star-citizen-helper
